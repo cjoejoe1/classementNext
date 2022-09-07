8acc91ffb1c9a03e3e64a8f6715789c1bd9abad5
John-Pierre Clark <jpc@johnpierreclark.com>
2022-09-07 16:38:48 +0100

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
         return (
         <Html lang="en">
             <Head>
-      <link rel="shortcut icon" href="../static/favicon.png" />
+      <link rel="shortcut icon" href="/static/favicon.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,700;0,800;1,100;1,300;1,500;1,600&display=swap" rel="stylesheet"></link>
