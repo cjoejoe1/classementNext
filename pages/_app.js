6963905f15d066ffc755b29f60932a91eb3edb23
John-Pierre Clark <jpc@johnpierreclark.com>
2022-09-10 14:30:35 +0100

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import { GlobalProvider } from "../context/GlobalContext";
 import "../node_modules/aos/dist/aos.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../assets/fonts/icon-font/css/style.css";
+import Script from 'next/script'
+
 function MyApp({ Component, pageProps }) {
 
   return  (
