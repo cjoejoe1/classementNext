c12ce48f0bb6ff22a33040c9127d1c5f8441de3e
John-Pierre Clark <jpc@johnpierreclark.com>
2022-09-11 16:32:17 +0100

    gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import Script from 'next/script'
+
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
@@ -33,12 +35,7 @@ export default class MyDocument extends Document {
         return (
         <Html lang="en">
             <Head>
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,700;0,800;1,100;1,300;1,500;1,600&display=swap" rel="stylesheet"></link>
-        <meta name="facebook-domain-verification" content="cgg6ot1j65djjforxhir374igcqpqa" />
-        <script dangerouslySetInnerHTML={{
+            <script dangerouslySetInnerHTML={{
     __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -46,9 +43,18 @@ export default class MyDocument extends Document {
   })(window,document,'script','dataLayer','GTM-TD4XDXH');`,
   }}>
   </script>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,700;0,800;1,100;1,300;1,500;1,600&display=swap" rel="stylesheet"></link>
+        <meta name="facebook-domain-verification" content="cgg6ot1j65djjforxhir374igcqpqa" />
+        
     </Head>
             <body>
-            
+            <Script>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+</Script>
             <Main />
             <NextScript />
             </body>
