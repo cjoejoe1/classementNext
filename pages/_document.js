cf783f2994137950545c4fcd0872d8814f8c95f5
John-Pierre Clark <jpc@johnpierreclark.com>
2022-09-11 14:49:12 +0100

    gtm

--- pages/_document.js
+++ pages/_document.js
@@ -38,10 +38,17 @@ export default class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,700;0,800;1,100;1,300;1,500;1,600&display=swap" rel="stylesheet"></link>
         <meta name="facebook-domain-verification" content="cgg6ot1j65djjforxhir374igcqpqa" />
+        <script dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-TD4XDXH');`,
+  }}>
+  </script>
     </Head>
             <body>
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD4XDXH"
-height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+            
             <Main />
             <NextScript />
             </body>
