cc71da09fc397d259a35310c0fa210e84001747f
John-Pierre Clark <jpc@johnpierreclark.com>
2022-09-11 14:16:07 +0100

    domainverify

--- pages/_document.js
+++ pages/_document.js
@@ -37,6 +37,7 @@ export default class MyDocument extends Document {
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,700;0,800;1,100;1,300;1,500;1,600&display=swap" rel="stylesheet"></link>
+        <meta name="facebook-domain-verification" content="cgg6ot1j65djjforxhir374igcqpqa" />
     </Head>
             <body>
             <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD4XDXH"
